91eb0883d9c56a9eff6d21bdbebf3d1c017952b5
dwijpr <dwijpr@gmail.com>
2016-04-17 12:31:50 +0700

    config app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
-    <!-- Styles -->
     {!! Html::style('thirdparty/bootstrap/3.3.6/css/bootstrap.css') !!}
 
     <style>
@@ -27,17 +24,13 @@
 
     @yield ('styles')
     
-    <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     {!! Html::script('thirdparty/bootstrap/3.3.6/js/bootstrap.js') !!}
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                     <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
@@ -45,16 +38,13 @@
                     <span class="icon-bar"></span>
                 </button>
 
-                <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    {{ config('app.name') }}
                 </a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
@@ -88,8 +78,6 @@
             </div>
         </div>
     </nav>
-
     @yield('content')
-
 </body>
 </html>
